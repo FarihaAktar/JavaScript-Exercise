633fce504a85941c2a0fb3112d51a3634cfcc998
Farihaakter <yellowfariha@gmail.com>
2021-09-30 19:10:39 +0600

    first commit

---
+++ Index.js
@@ -0,0 +1,69 @@
+// const CountVowelCount = (x) => {
+//     let arr = ["a", "e", "i", "o ","u"];
+//     let res = []
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i] === arr){
+//             console.log(res.push(x[i]))
+//         }
+//         else{
+
+//         }
+
+//     }
+//     return res;
+// }
+
+// const res = CountVowelCount("Hello World")
+// console.log(res)
+
+// const a = {x: 12}
+// const b = a
+// const c = {x : 13}
+// const d = {...c}
+// console.log(a === b)
+// console.log(c === d)
+// console.log(a === d)
+
+// const str = 'hello world'
+// console.log(str.length)
+
+// const vowels = ['a', 'e', 'i', 'o', 'u']
+// console.log(vowels.includes('b'))
+
+const countVowels = (str) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+
+        if (vowels.includes(str[i])) {
+            count = count + 1
+        }
+
+    }
+    return count
+}
+
+const res = countVowels("aeiou")
+// console.log(res)
+
+// countVowels("Romel + Fariha")
+
+const reverseArray = (arr) => {
+    const newArr = []
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            const value = arr[i];
+            newArr.unshift(value)
+
+        }
+        console.log(newArr);
+    } else {
+        console.log("Enter an array")
+    }
+
+
+
+}
+reverseArray([])
+// const arr = [1, 2, 3]
+// console.log(Array.isArray(arr))
