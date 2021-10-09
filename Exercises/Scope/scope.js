dad9fc18d3ad255e597e6f5bc3a62d0903b286b4
Farihaakter <yellowfariha@gmail.com>
2021-10-09 14:43:06 +0600

    added scope

---
+++ Exercises/Scope/scope.js
@@ -0,0 +1,21 @@
+// var a = 2;
+
+// function test(){
+//     //  var a = 5
+//     // console.log(a)
+    
+// }
+// test()
+function c (){
+    var a = 1
+    console.log(a)
+}
+
+function test(){
+    a = 5
+    console.log(a)
+    
+}
+c()
+test()
+console.log(a)
