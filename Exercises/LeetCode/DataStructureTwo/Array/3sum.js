4fd88149282670edd67729030c7ac56d4143d18c
Farihaakter <yellowfariha@gmail.com>
2021-10-25 15:08:12 +0600

    added solutions

--- Exercises/LeetCode/DataStructureTwo/Array/3sum.js
+++ Exercises/LeetCode/DataStructureTwo/Array/3sum.js
@@ -1,34 +1,41 @@
-const arr = [-1,0,1,2,-1,-4]
+const arr = [-1, 0, 1, 2, -1, -4];
+const arr2 = [3, 0, -2, -1, 1, 2]
+// console.log(arr2.length)
 
-const threeSum = (arr) =>{
-    var target = 0;
-    if (nums.length === 3) {
-        if (nums[0]+nums[1]+nums[2] === 0) {
-            return [[nums[0],nums[1],nums[2]]];
+const threeSum = (nums) => {
+    const triplets = [];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] > 0 || nums[nums.length - 1] < 0) {
+            break
         }
-    }
-    
-    var results = [];
-    var hashMap = {};
-    
-    for (var i=0; i<nums.length; i++) {
-        for (var j=i+1; j<nums.length; j++) {
-            for (var k=j+1; k<nums.length; k++) {
-              if (nums[i]+nums[j]+nums[k] === target) {
-                if (!hashMap[nums[i]*nums[j]*nums[k]]) {
-                    results.push([nums[i],nums[j],nums[k]]);
-                    results[results.length-1].sort();
-                    hashMap[nums[i]*nums[j]*nums[k]] = true;
-                    //console.log(hashMap);
+        if(i > 0 && nums[i] === nums[i-1]) continue;
+
+        let leftPointer = i + 1 , rightPointer = nums.length -1;
+        while(leftPointer < rightPointer){
+            const sum = nums[i] + nums[leftPointer] + nums[rightPointer];
+
+            if(sum > 0) rightPointer -=1
+            else if(sum< 0) leftPointer +=1;
+            else{
+                triplets.push([nums[i],  nums[leftPointer] , nums[rightPointer]]);
+                while(leftPointer < rightPointer && nums[leftPointer] === nums[leftPointer + 1]) {
+                    leftPointer += 1;
                 }
-              }
-              
+                while(leftPointer < rightPointer && nums[rightPointer] === nums[rightPointer - 1]){
+                    rightPointer -= 1;
+                }
+                leftPointer += 1;
+                rightPointer -= 1;
             }
         }
+
     }
-    return results;
+    return triplets;
 }
 
-const res = threeSum(arr);
+const res = threeSum(arr2);
 
 console.log(res)
+
+
